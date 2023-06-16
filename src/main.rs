e5025161164c89554b34e2dc61bd3ac0091557ed
Nikita Chulkov <nikita.chulkov@sakhalin2.ru>
2023-06-16 12:01:05 +1100

    Fixed bug with wrong order of actions is string decoding loop

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,18 @@ fn decode_string(string: &str) -> Option<String> {
     let mut is_parsing_hex = false;
 
     for c in string.chars() {
+        if c == char::from_u32(0x01).unwrap() {
+            is_parsing_hex = true;
+            continue;
+        }
+
+        if !is_parsing_hex {
+            res.push(c);
+            continue;
+        }
+
+        buf.push(c);
+
         if buf.len() == 4 {
             is_parsing_hex = false;
 
@@ -47,18 +59,6 @@ fn decode_string(string: &str) -> Option<String> {
             res.push_str(&decoded);
             buf = String::new();
         }
-
-        if !is_parsing_hex && c != char::from_u32(0x01).unwrap() {
-            res.push(c);
-            continue;
-        }
-
-        if c == char::from_u32(0x01).unwrap() {
-            is_parsing_hex = true;
-            continue;
-        }
-
-        buf.push(c);
     }
     return Some(res);
 }
